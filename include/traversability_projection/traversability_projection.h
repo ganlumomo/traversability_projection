929faafb884b8c0762ae7129ebe634253c613abd
Lu Gan <ganlu@umich.edu>
2020-03-08 03:45:29 -0400

    working on kitti

--- include/traversability_projection/traversability_projection.h
+++ include/traversability_projection/traversability_projection.h
@@ -7,6 +7,7 @@
 #include <tf/transform_listener.h>
 #include <pcl_ros/transforms.h>
 #include <sensor_msgs/point_cloud_conversion.h>
+#include <pcl_conversions/pcl_conversions.h>
 
 #include <grid_map_msgs/GetGridMap.h>
 #include <grid_map_msgs/GetGridMapInfo.h>
@@ -21,31 +22,38 @@
 
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
-#include <opencv2/calib3d.hpp>
+
+#include <queue>
 
 class TraversabilityProjection {
   public:
-    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::CameraInfo> MySyncPolicy;
+    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> MySyncPolicy;
     
     TraversabilityProjection(ros::NodeHandle& nh)
       : nh_(nh)
       , gridmap_set_(false) {
-        color_sub_ = new message_filters::Subscriber<sensor_msgs::Image> (nh_, "/camera/color/image_raw", 10);
-        depth_sub_ = new message_filters::Subscriber<sensor_msgs::Image> (nh_, "/camera/aligned_depth_to_color/image_raw", 10);
-        camera_info_sub_ = new message_filters::Subscriber<sensor_msgs::CameraInfo> (nh_, "/camera/color/camera_info", 10);
-        sync_ = new message_filters::Synchronizer<MySyncPolicy> (MySyncPolicy(100), *color_sub_, *depth_sub_, *camera_info_sub_);
-        sync_->registerCallback(boost::bind(&TraversabilityProjection::colorDepthCallback, this, _1, _2, _3));
+        color_sub_ = new message_filters::Subscriber<sensor_msgs::Image> (nh_, "/kitti_player_mini_node/left_color_image", 10);
+        depth_sub_ = new message_filters::Subscriber<sensor_msgs::Image> (nh_, "/kitti_player_mini_node/depth_image", 10);
+        //camera_info_sub_ = new message_filters::Subscriber<sensor_msgs::CameraInfo> (nh_, "/camera/color/camera_info", 10);
+        //sync_ = new message_filters::Synchronizer<MySyncPolicy> (MySyncPolicy(100), *color_sub_, *depth_sub_, *camera_info_sub_);
+        //sync_->registerCallback(boost::bind(&TraversabilityProjection::colorDepthInfoCallback, this, _1, _2, _3));
+        sync_ = new message_filters::Synchronizer<MySyncPolicy> (MySyncPolicy(100), *color_sub_, *depth_sub_);
+        sync_->registerCallback(boost::bind(&TraversabilityProjection::colorDepthCallback, this, _1, _2));
         cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2> ("test_points", 1);
         gridmap_sub_ = nh_.subscribe("/traversability_estimation/traversability_map", 1, &TraversabilityProjection::gridmapCallback, this);
         
       }
 
+    void colorDepthInfoCallback(const sensor_msgs::ImageConstPtr& color_msg,
+                                const sensor_msgs::ImageConstPtr& depth_msg,
+                                const sensor_msgs::CameraInfoConstPtr& camera_info_msg);
+
     void colorDepthCallback(const sensor_msgs::ImageConstPtr& color_msg,
-                            const sensor_msgs::ImageConstPtr& depth_msg,
-                            const sensor_msgs::CameraInfoConstPtr& camera_info_msg);
+                            const sensor_msgs::ImageConstPtr& depth_msg);
 
     void gridmapCallback(const grid_map_msgs::GridMap& gridmap_msg);
 
+    void project(int height, int width);
   private:
     ros::NodeHandle nh_;
     message_filters::Subscriber<sensor_msgs::Image>* color_sub_;
@@ -57,4 +65,6 @@ class TraversabilityProjection {
     ros::Subscriber gridmap_sub_;
     grid_map::GridMap gridmap_;
     bool gridmap_set_;
+    std::queue<sensor_msgs::Image> color_queue;
+    std::queue<sensor_msgs::PointCloud2> pt_queue;
 };
--- src/traversability_projection.cpp
+++ src/traversability_projection.cpp
@@ -1,8 +1,100 @@
 #include "traversability_projection.h"
 
 void TraversabilityProjection::colorDepthCallback(const sensor_msgs::ImageConstPtr& color_msg,
-                                                  const sensor_msgs::ImageConstPtr& depth_msg,
-                                                  const sensor_msgs::CameraInfoConstPtr & camera_info_msg) {
+                                                  const sensor_msgs::ImageConstPtr& depth_msg) {
+
+
+  float cx_ = 601.8873;
+  float cy_ = 183.1104;
+  float fx_ = 707.0912;
+  float fy_ = 707.0912;
+  
+  cv_bridge::CvImagePtr cv_ptr;
+  cv_ptr = cv_bridge::toCvCopy(depth_msg, sensor_msgs::image_encodings::TYPE_16UC1);
+  cv::Mat depth_img = cv_ptr->image;
+
+  int width = depth_img.cols;
+  int height = depth_img.rows;
+
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+  for (int32_t i = 0; i < width * height; ++i) {
+    int ux = i % width;
+    int uy = i / width;
+
+    float pix_depth = (float) depth_img.at<uint16_t>(uy, ux) / 1000;
+    pcl::PointXYZ pt;
+    pt.x = (ux - cx_) * (1.0 / fx_) * pix_depth;
+    pt.y = (uy - cy_) * (1.0 / fy_) * pix_depth;
+    pt.z = pix_depth;
+    cloud.points.push_back(pt);
+  }
+  sensor_msgs::PointCloud2 cloud_msg;
+  pcl::toROSMsg(cloud, cloud_msg);
+  cloud_msg.header.frame_id = "robot";
+ 
+  sensor_msgs::PointCloud cloud1;
+  sensor_msgs::convertPointCloud2ToPointCloud(cloud_msg, cloud1);
+
+  // Fetch tf transform
+  try {
+    tfListener_.transformPointCloud("/map",
+                              cloud_msg.header.stamp,
+                              cloud1,
+                              cloud_msg.header.frame_id,
+                              cloud1);
+  } catch (tf::TransformException ex) {
+    ROS_ERROR("%s",ex.what());
+    return;
+  }
+  sensor_msgs::convertPointCloudToPointCloud2(cloud1, cloud_msg);
+  cloud_msg.header.frame_id = "/map";
+
+  pt_queue.push(cloud_msg);
+  color_queue.push(*color_msg);
+
+  project(width, height);
+}
+
+void TraversabilityProjection::project(int width, int height) {
+  std::cout << pt_queue.size() << std::endl;
+  std::cout << color_queue.size() << std::endl;
+  if (pt_queue.size() >= 40 && gridmap_set_) {
+
+    sensor_msgs::PointCloud2 cloud2 = pt_queue.front();
+    pt_queue.pop();
+    cloud_pub_.publish(cloud2);
+
+    sensor_msgs::PointCloud cloud;
+    sensor_msgs::convertPointCloud2ToPointCloud(cloud2, cloud);
+    sensor_msgs::Image color_msg = color_queue.front();
+    color_queue.pop();
+
+   // cv::Mat proj_img(cv::Size(camera_info_msg->width, camera_info_msg->height), CV_32FC1);
+    
+    cv::Mat proj_img(cv::Size(width, height), CV_8UC3, cv::Scalar(255, 255, 255));
+    for (size_t i = 0; i < height; ++i) {
+      for (size_t j = 0; j < width; ++j) {
+        grid_map::Index index;
+        grid_map::Position position(cloud.points[i*width + j].x,
+                                    cloud.points[i*width + j].y);
+        if (!gridmap_.getIndex(position, index))
+          continue;
+        float traversability = gridmap_.at("traversability", index);
+        //proj_img.at<float>(i, j) = traversability;
+        proj_img.at<cv::Vec3b>(i, j) = cv::Vec3b(255*traversability, 0, 0);
+      }
+    }
+
+    cv_bridge::CvImagePtr cv_ptr;
+    cv_ptr = cv_bridge::toCvCopy(color_msg, sensor_msgs::image_encodings::RGB8);
+    cv::Mat image = cv_ptr->image;
+    cv::addWeighted(image, 0.5, proj_img,0.5,0, proj_img);
+    cv::imwrite("test.png", proj_img);
+  }
+}
+void TraversabilityProjection::colorDepthInfoCallback(const sensor_msgs::ImageConstPtr& color_msg,
+                                                      const sensor_msgs::ImageConstPtr& depth_msg,
+                                                      const sensor_msgs::CameraInfoConstPtr & camera_info_msg) {
   
   image_geometry::PinholeCameraModel camera_model;
   camera_model.fromCameraInfo(camera_info_msg);
@@ -32,8 +124,8 @@ void TraversabilityProjection::colorDepthCallback(const sensor_msgs::ImageConstP
     return;
   }
   sensor_msgs::convertPointCloudToPointCloud2(cloud, *cloud_msg);
-  //cloud_msg->header.frame_id = "/odom";
-  //cloud_pub_.publish(*cloud_msg);
+  cloud_msg->header.frame_id = "/odom";
+  cloud_pub_.publish(*cloud_msg);
   //
   if (gridmap_set_) {
    // cv::Mat proj_img(cv::Size(camera_info_msg->width, camera_info_msg->height), CV_32FC1);
@@ -47,7 +139,10 @@ void TraversabilityProjection::colorDepthCallback(const sensor_msgs::ImageConstP
           continue;
         float traversability = gridmap_.at("traversability", index);
         //proj_img.at<float>(i, j) = traversability;
-        proj_img.at<cv::Vec3b>(i, j) = cv::Vec3b(255*traversability, 0, 0);
+        if (traversability > 0.5)
+          proj_img.at<cv::Vec3b>(i, j) = cv::Vec3b(255, 0, 0);
+        else
+          proj_img.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 0, 0);
       }
     }
 
